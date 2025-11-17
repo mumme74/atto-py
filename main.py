d08d237e1ebd27ed57aeb245a5d9083eae9953d9
Fredrik Johansson <fredrik@mumme.se>
2025-11-17 09:11:59 +0100

    Add ability to execute scripts from main file.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """The main entrypoint for the atto-py interpreter"""
 
 from argparse import ArgumentParser
 from pathlib import Path
 
+from src.interpreter import Interpreter
+
+def run_interpreter(script: Path):
+    """Runs the interpreter"""
+    interp = Interpreter()
+    interp.exec_file(script)
+
 def main():
+    """Main function of our interpreter program"""
     argparser = ArgumentParser()
     argparser.add_argument("file", help="The atto-script to run")
     args = argparser.parse_args()
-    script = Path(__file__).parent.absolute() / args.file
-    print(f"File to run: {script}")
 
+    # handle other argv options in the future
+
+    script = Path(__file__).parent.absolute() / args.file
+    run_interpreter(script)
 
 if __name__ == "__main__":
     main()
